dd46c0f32bcea09bccc2e08daa6221c7a3af1a4e
Jaka Potokar <cc.jakapotokar@gmail.com>
2024-10-15 13:22:06 +0200

    page transition update

--- FrontEnd/my-app/src/components/PageTransition.tsx
+++ FrontEnd/my-app/src/components/PageTransition.tsx
@@ -1,20 +1,23 @@
 "use client";
 import { usePageTransition } from "@/components/PageTransitionContext";
+import { usePathname } from "next/navigation";
 
 export default function PageTransition() {
   const { showTransition } = usePageTransition();
 
+  const pathname = usePathname();
+
   return (
     <>
       <div
-        className={`absolute top-[11vh] w-[100vw] topShadow z-40 ${
-          showTransition && "topTransition"
-        }`}
+        className={`absolute top-[11vh] w-[100vw] ${
+          pathname.includes("positions") ? "topShadowYellow" : "topShadow"
+        } z-40 ${showTransition && "topTransition"}`}
       ></div>
       <div
-        className={`absolute bottom-0 w-[100vw] bottomShadow z-40 ${
-          showTransition && "bottomTransition"
-        }`}
+        className={`absolute bottom-0 w-[100vw] ${
+          pathname.includes("positions") ? "bottomShadowYellow" : "bottomShadow"
+        } z-40 ${showTransition && "bottomTransition"}`}
       ></div>
     </>
   );
