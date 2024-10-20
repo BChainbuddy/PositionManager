b7f1115b65005f3c02e1163daf929512be9f5cc9
Jaka Potokar <cc.jakapotokar@gmail.com>
2024-10-20 02:21:14 +0200

    new animation for forgeCard

--- FrontEnd/my-app/src/components/ForgeCard.tsx
+++ FrontEnd/my-app/src/components/ForgeCard.tsx
@@ -7,8 +7,6 @@ export default function ForgeCard() {
   const [step, setStep] = useState<number>(0);
 
   return (
-    <div className="w-96 h-96 bg-[#002114] forgeCard">
-      {step == 0 ? <Step1 /> : <></>}
-    </div>
+    <div className="w-96 h-96 forgeCard">{step == 0 ? <Step1 /> : <></>}</div>
   );
 }
