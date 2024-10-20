e0f4da30e31e7b62013d48dc0c43ddc4f31f5407
Jaka Potokar <cc.jakapotokar@gmail.com>
2024-10-20 20:47:45 +0200

    added Card2 component and basic setup

--- FrontEnd/my-app/src/components/ForgeCard.tsx
+++ FrontEnd/my-app/src/components/ForgeCard.tsx
@@ -2,11 +2,14 @@
 
 import { useState } from "react";
 import Step1 from "./Step1";
+import Step2 from "./Step2";
 
 export default function ForgeCard() {
-  const [step, setStep] = useState<number>(0);
+  const [step, setStep] = useState<number>(1);
 
   return (
-    <div className="w-96 h-96 forgeCard">{step == 0 ? <Step1 /> : <></>}</div>
+    <div className="w-96 h-96 forgeCard">
+      {step == 0 ? <Step1 /> : 1 ? <Step2 /> : <></>}
+    </div>
   );
 }
---
+++ FrontEnd/my-app/src/components/Step2.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Step2() {
+  const [automatic, setAutomatic] = useState(true);
+
+  return (
+    <div className="h-full w-full flex flex-col text-white justify-center items-center font-juraBold">
+      <p className="text-center text-[#01FF39] text-xl">CHOOSE LIQUIDITY</p>
+      <div className="flex flex-row space-x-2">
+        <p>Automatic</p>
+        <div
+          className={` w-[3rem] h-[1.5rem] relative z-10 rounded-xl flex items-center transition-all duration-1000 ease-in-out ${
+            automatic ? "bg-green-400" : "bg-green-900"
+          }`}
+        >
+          <div
+            className={`absolute z-20 rounded-full h-[1.3rem] w-[1.3rem] bg-white transition-all duration-1000 ease-in-out cursor-pointer ${
+              automatic ? "translate-x-[1.6rem]" : "translate-x-[0.1rem]"
+            }`}
+            onClick={() => setAutomatic(!automatic)}
+          ></div>
+        </div>
+      </div>
+      <button className="flex items-center justify-center h-8 w-24 bg-[#01FF39] rounded-2xl text-black mt-6">
+        CONTINUE
+      </button>
+      <button className="text-sm flex items-center justify-center h-6 w-12 bg-white text-black rounded-2xl mt-2">
+        back
+      </button>
+    </div>
+  );
+}
