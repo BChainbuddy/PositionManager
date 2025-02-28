827cf2fcd9e4dbaa0eeefade282edfa5b8ca3b7b
Jaka Potokar <cc.jakapotokar@gmail.com>
2025-02-28 15:18:19 +0100

    add wallet token transfer UI

--- FrontEnd/my-app/src/components/Wallet/WalletModal.tsx
+++ FrontEnd/my-app/src/components/Wallet/WalletModal.tsx
@@ -11,12 +11,22 @@ interface WalletModalProps {
   setViewModal: (show: boolean) => any;
 }
 
+interface Token {
+  symbol: string;
+  balance: number;
+  address: string;
+  name: string;
+  decimals: number;
+  logo: string;
+}
+
 export default function WalletModal({
   showModal,
   setSigner,
   setViewModal,
 }: WalletModalProps) {
   const [transfer, setTransfer] = useState<boolean>(false);
+  const [tokens, setTokens] = useState<Token[]>([]);
 
   const wallet = useActiveWallet();
   const account = useActiveAccount();
@@ -32,6 +42,17 @@ export default function WalletModal({
       }
       const data = await response.json();
       console.log(data);
+      const tokenArray: Token[] = data.data.map((token: any) => {
+        return {
+          symbol: token.symbol,
+          balance: token.balance,
+          address: token.address,
+          name: token.name,
+          decimals: token.decimals,
+          logo: token.logo,
+        };
+      });
+      setTokens(tokenArray);
     }
     if (account?.address) {
       getBalances();
@@ -48,7 +69,7 @@ export default function WalletModal({
       >
         <h1 className="text-3xl font-juraBold">WALLET</h1>
         {transfer ? (
-          <div></div>
+          <Transfer setIsVisible={setTransfer} tokens={tokens} />
         ) : (
           <ActionButton text="Transfer" onClick={() => setTransfer(true)} />
         )}
@@ -66,14 +87,22 @@ export default function WalletModal({
 
 const Transfer = ({
   setIsVisible,
+  tokens,
 }: {
   setIsVisible: (is: boolean) => void;
+  tokens: Token[];
 }) => {
   const [address, setAddress] = useState<string>("");
-  const [amount, setAmount] = useState<number>(0);
+  const [amount, setAmount] = useState<number>();
+  const [selectedToken, setSelectedToken] = useState<Token | null>(null);
 
   return (
-    <div className="flex flex-col gap-y-5">
+    <div className="flex flex-col gap-y-5 mb-2">
+      <TokenSelector
+        tokens={tokens}
+        setSelectedToken={setSelectedToken}
+        selectedToken={selectedToken}
+      />
       <Input
         label="Address"
         placeholder="0xd2fdd21AC3553Ac578a69a64F833788f2581BF05"
@@ -87,27 +116,73 @@ const Transfer = ({
         setValue={setAmount}
       />
       <ActionButton text="Transfer" onClick={() => {}} />
-      <Back
-        onClick={() => {
-          setIsVisible(false);
-        }}
+      <ActionButton
+        text="Back"
+        className="w-[4rem]"
+        onClick={() => setIsVisible(false)}
       />
     </div>
   );
 };
 
-const TokenSelector = () => {};
+const TokenSelector = ({
+  tokens,
+  setSelectedToken,
+  selectedToken,
+}: {
+  tokens: Token[];
+  setSelectedToken: (token: Token) => void;
+  selectedToken: Token | null;
+}) => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  return (
+    <div className="relative flex mt-5">
+      <button
+        className="w-[8rem] h-[2rem] border-2 border-white flex justify-center items-center rounded-xl mx-auto cursor-pointer"
+        onClick={() => setOpen(!open)}
+      >
+        <p>{selectedToken ? selectedToken.symbol : "Select Token"}</p>
+      </button>
+      {open && (
+        <div className="absolute gap-y-5 border-white border-2 bg-black p-5 rounded-xl z-50 w-full">
+          <div className="flex flex-row justify-between mb-1">
+            <p className="text-sm text-white font-juraBold">Symbol</p>
+            <p className="text-sm text-white font-juraBold">Balance</p>
+          </div>
+          {tokens.map((token, i) => (
+            <div
+              key={i}
+              className="flex flex-row justify-between cursor-pointer mt-1"
+              onClick={() => {
+                setSelectedToken(token);
+                setOpen(false);
+              }}
+            >
+              <p>{token.symbol}</p>
+              <p>{token.balance / 10 ** token.decimals}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
 const ActionButton = ({
   text,
   onClick,
+  className,
 }: {
   text: string;
   onClick: () => void;
+  className?: string;
 }) => {
   return (
     <button
-      className="w-[8rem] h-[2rem] border-2 border-white flex justify-center items-center rounded-xl mx-auto cursor-pointer mt-5"
+      className={`h-[2rem] border-2 border-white flex justify-center items-center rounded-xl mx-auto cursor-pointer ${
+        className ? className : "w-[8rem]"
+      }`}
       onClick={onClick}
     >
       {text}
@@ -128,24 +203,13 @@ const Input = ({
 }) => {
   return (
     <div className="flex flex-col">
-      <p>{label}</p>
+      <p className="font-juraBold text-sm">{label}</p>
       <input
-        className="w-[10rem] h-[2rem] border-2 border-white rounded-xl mx-auto"
+        className="w-[12rem] rounded-lg mx-auto px-2 py-1 text-black outline-none focus:outline-green-500 focus:outline-1 text-sm"
         placeholder={placeholder}
         value={value}
         onChange={(e) => setValue(e.target.value)}
       />
     </div>
-  );
-};
-
-const Back = ({ onClick }: { onClick: () => void }) => {
-  return (
-    <button
-      className="w-[8rem] h-[2rem] border-2 border-white flex justify-center items-center rounded-xl mx-auto cursor-pointer mt-5"
-      onClick={onClick}
-    >
-      Back
-    </button>
   );
 };
