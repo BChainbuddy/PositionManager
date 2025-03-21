7164962c0006682a04b92b55a705aded65bb0fee
Jaka Potokar <cc.jakapotokar@gmail.com>
2025-03-21 11:15:20 +0100

    make carousels responsive on small screens

--- FrontEnd/my-app/src/components/Home/LogoCarousel.tsx
+++ FrontEnd/my-app/src/components/Home/LogoCarousel.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import { useEffect } from "react";
 
-export default function LogoCarousel({ up }: { up: boolean }) {
+export default function LogoCarousel({ up }: { up?: boolean }) {
   useEffect(() => {
     const scrollers = up
       ? document.querySelectorAll(".scrollerUp")
@@ -20,6 +20,7 @@ export default function LogoCarousel({ up }: { up: boolean }) {
         element.setAttribute("data-animated", "true");
 
         const selector = up ? ".carouselUp" : ".carouselDown";
+
         const scrollerInner = element.querySelector(selector) as HTMLElement;
         if (scrollerInner) {
           const scrollerContent = Array.from(scrollerInner.children);
@@ -38,17 +39,19 @@ export default function LogoCarousel({ up }: { up: boolean }) {
   return (
     <div
       className={`overflow-hidden max-h-[100vh] absolute ${
-        up ? "top-0 left-[20%] scrollerUp" : "bottom-0 right-[20%] scrollerDown"
+        up
+          ? "md:top-0 md:left-[20%] left-0 top-[10%] scrollerUp"
+          : "md:bottom-0 md:right-[20%] right-0 bottom-[10%] scrollerDown"
       }`}
     >
       <div
-        className={`flex flex-col gap-[1rem] h-[200%] ${
+        className={`flex md:flex-col flex-row gap-[1rem] md:h-[200%] md:w-fit w-[200%] h-[100%] ${
           up ? "carouselUp" : "carouselDown"
-        } `}
+        }`}
       >
         {[...Array(20)].map((_, i) => (
           <div
-            className="relative h-16 flex items-center justify-center"
+            className="relative md:h-16 md:w-auto h-auto w-8 flex items-center justify-center"
             key={i}
           >
             <Image
@@ -56,14 +59,14 @@ export default function LogoCarousel({ up }: { up: boolean }) {
                 [
                   "/tokenImages/binance-logo.svg",
                   "/tokenImages/bitcoin-logo.svg",
-                  "/tokenImages/ethereum-logo.png",
+                  "/tokenImages/ethereum-logo2.png",
                   "/tokenImages/solana-logo.svg",
                 ][i % 4]
               }`}
               alt={`Logo ${i}`}
               width={128}
               height={64}
-              className="h-16 w-auto object-contain"
+              className="md:h-16 md:w-auto w-8 h-auto object-contain"
             />
           </div>
         ))}
