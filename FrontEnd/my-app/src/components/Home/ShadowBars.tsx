cb87a15bb42cc1c419df0650f66702411597f482
Jaka Potokar <cc.jakapotokar@gmail.com>
2025-03-21 11:30:23 +0100

    make shadow bars responsive

--- FrontEnd/my-app/src/components/Home/ShadowBars.tsx
+++ FrontEnd/my-app/src/components/Home/ShadowBars.tsx
@@ -1,8 +1,8 @@
 export default function ShadowBars() {
   return (
     <>
-      <div className="absolute -left-[60px] top-0 h-[100vh] w-[60px] shadow-[0_0_2rem_2rem_#01FF39]"></div>
-      <div className="absolute right-[-60px] top-0 h-[100vh] w-[60px] shadow-[0_0_2rem_2rem_#01FF39]"></div>
+      <div className="absolute -left-[60px] w-[60px] top-0 h-[100vh] md:shadow-[0_0_2rem_2rem_#01FF39] shadow-[0_0_1rem_1rem_#01FF39]"></div>
+      <div className="absolute right-[-60px] top-0 h-[100vh] w-[60px] md:shadow-[0_0_2rem_2rem_#01FF39] shadow-[0_0_1rem_1rem_#01FF39]"></div>
     </>
   );
 }
