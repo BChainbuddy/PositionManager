f9d0411710a30385609111af6a4c071d97574c29
Jaka Potokar <cc.jakapotokar@gmail.com>
2025-03-23 13:21:51 +0100

    make footer responsive on sm

--- FrontEnd/my-app/src/components/Home/Footer.tsx
+++ FrontEnd/my-app/src/components/Home/Footer.tsx
@@ -1,13 +1,17 @@
 export default function Footer() {
   return (
-    <div className="flex flex-row mt-44 justify-evenly items-center text-center bg-[#01FF39] py-[1.2rem] text-sm">
-      <p className="text-black w-[15rem]">
+    <div className="flex flex-col sm:flex-row mt-16 md:mt-44 justify-evenly items-center text-center bg-[#01FF39] py-4 md:text-sm text-xs">
+      <p className="text-black md:w-[15rem]">
         Powered by Jaka Potokar a.k.a. BChainBuddy
       </p>
-      <p>© 2024 Trade Forge. All rights reserved.</p>
-      <p className="text-black w-[15rem]">
+      <p className="my-2 md:my-0">© 2024 Trade Forge. All rights reserved.</p>
+      <p className="text-black sm:w-[15rem]">
         Explore more projects on GitHub:{" "}
-        <a href="https://github.com/BchainBuddy" target="_blank">
+        <a
+          href="https://github.com/BchainBuddy"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           github.com/BChainBuddy
         </a>
       </p>
